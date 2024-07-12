4bb710e343cd5251cfddf1784d298a39406f01c1
kirill <racconrolly@gmail.com>
2024-07-12 19:39:03 +0300

    Front

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,59 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Newtonsoft.Json;
 
 namespace WeatherApp_API
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private readonly string apiKey = "67ba5ab753b84f85b07141755230405";
+        private string cityName;
+
+        private DateTime _now;
+        public DateTime Now
+        {
+            get { return _now; }
+            set{ 
+                _now = value;
+                OnPropertyChanged(nameof(Now));
+                }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += (sender, args) =>
+            {
+                Now = DateTime.Now;
+
+            };
+            timer.Start();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            { this.DragMove(); }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGetWeather_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
